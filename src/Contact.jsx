ba17b32d8483abf86d3edd895b7e67ca5d42d344
D <danyyounan@gmail.com>
2023-08-28 16:12:20 -0400

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
         <div>
             <h1>Contact</h1>
-            <p>Get in touch with us!</p>
+            <p>Get in touch!</p>
         </div>
     )
 }
